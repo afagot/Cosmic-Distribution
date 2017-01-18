50502507bc0eb0a7a76372e45b6b6db5eb8ae1a7
afagot <alexis.fagot@ugent.be>
2017-01-18 11:40:14 +0100

    Code to study the effect of the orientation of a trigger system on the particle distribution on the surface of a vertical or horizontal detector plane

---
+++ include/fonctions.h
@@ -0,0 +1,171 @@
+//******************************************************
+//*      Cosmic Muons Monte Carlo Simulation           *
+//*                                                    *
+//*      Alexis FAGOT                                  *
+//*      alexis.fagot@ugent.be                         *
+//*                                                    *
+//*      Janvier 16, 2016                              *
+//*                                                    *
+//*      HEADER FILE                                   *
+//******************************************************
+
+
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+//Define necessary variables
+const double __scint_L = 32.; //scintillator «L»ength
+const double __scint_W = 9.4; //scintillator «W»idth
+const double __scint_H = 20.; //top scintillator plane «H»eight
+const double __scint_D = 4.;  //«D»istance in between the sintillator planes
+const double __scint_S = 20.; //Distance to the «S»creen (vertical distribution)
+
+//********************************************************************************************
+
+//Function to cast an integer into a string
+string intTostring(int value){
+    string word;
+    stringstream ss;
+    ss << setw(2) << setfill('0') << value;
+    ss >> word;
+
+    return word;
+}
+
+//********************************************************************************************
+
+//Definition of Coordinates
+struct Point{
+    double x;
+    double y;
+    double z;
+};
+typedef Point Vector;
+
+//A (theta,phi) angle pair.
+typedef std::pair<double,double> Direction;
+
+//********************************************************************************************
+
+//pi Definition
+const double PI = 4*atan(1);
+
+//********************************************************************************************
+
+//function that builds a vector from 2 points
+Vector makeVector(Point A, Point B){
+    Vector AB;
+    AB.x = B.x - A.x;
+    AB.y = B.y - A.y;
+    AB.z = B.z - A.z;
+
+    return AB;
+}
+
+//********************************************************************************************
+
+//Shorthand notation to make the code more understadable and to allow for
+//drop-in replacement by another C++11 random number generator.
+typedef std::mt19937 Generator;
+
+//A fonction that generate a random position within a certain rectangle
+double getRandom(Generator& generator){
+    return generate_canonical<double, 32>(generator);
+}
+
+double getRandomInRange(Generator& generator, double x0, double length){
+    return length*getRandom(generator) + x0;
+}
+
+//********************************************************************************************
+
+//A fonction that generates a cos^2 cosmics distribution
+Direction getRandomDirection(Generator& generator){
+    double phi = 2*PI*getRandom(generator);
+
+    // Create cos²(theta) distribution
+    double theta = PI*getRandom(generator)/2.;
+    double test = getRandom(generator);
+
+    while (test > cos(theta)*cos(theta)){
+        theta = PI*getRandom(generator)/2.;
+        test = getRandom(generator);
+    }
+
+    return make_pair(theta, phi);
+}
+
+//Generate a random hit in the cosmic plane that is defined to be situated
+//right at the height of the lowest point of the scintillators via the
+//variable height
+Point getRandomMuonPosition(Generator& generator, double height){
+    Point muon;
+
+    muon.x = getRandomInRange(generator, -50., 50.);
+    muon.y = getRandomInRange(generator, -50., 50.);
+    muon.z = height;
+
+    return muon;
+}
+
+//********************************************************************************************
+
+//Compute the hit position in the scintillator planes knowing the equation of the planes
+//calculated by hand and using the origin of the muon, its direction (details on calculation
+//in the hand notebook)
+Point getHitPosition(const Point& P, const Direction& D, double alpha, int Doffset, string which){
+    Point I;
+
+    double scint_D = __scint_D + Doffset;
+    double t = 0.;
+
+    if(which == "TOP") t=((__scint_H-P.z)*cos(alpha)-P.y*sin(alpha))
+            /(sin(D.first)*sin(D.second)*sin(alpha)+cos(D.first)*cos(alpha));
+    if(which == "BOTTOM") t=((__scint_H-P.z)*cos(alpha)-P.y*sin(alpha)-scint_D)
+                          /(sin(D.first)*sin(D.second)*sin(alpha)+cos(D.first)*cos(alpha));
+    if(which == "GROUND") t = -P.z/cos(D.first);
+
+    if(which == "SCREEN") t = -(__scint_S+P.y)/(sin(D.first)*sin(D.second));
+
+    I.x = P.x + sin(D.first)*cos(D.second)*t;
+    I.y = P.y + sin(D.first)*sin(D.second)*t;
+    I.z = P.z + cos(D.first)*t;
+
+    return I;
+}
+
+//********************************************************************************************
+
+//Check if the muon is in the scintillator surfaces
+bool isInScint(const Point& P, const Direction& D, double alpha, int Doffset, string which){
+    Point I = getHitPosition(P,D,alpha,Doffset,which);
+
+    double scint_D = __scint_D + Doffset;
+
+    bool x_condition = ( I.x < (__scint_L/2.) && I.x > (-__scint_L/2.) );
+
+    double y_low = -__scint_W*cos(alpha)/2.;
+    double y_high = __scint_W*cos(alpha)/2.;
+
+    if(which == "BOTTOM"){
+        y_low -= scint_D/cos(alpha);
+        y_high -= scint_D/cos(alpha);
+    }
+    bool y_condition = ( I.y > y_low && I.y < y_high );
+
+    return (x_condition && y_condition);
+}
+
+#endif // FONCTIONS_H
---
+++ src/main.cc
@@ -0,0 +1,187 @@
+//******************************************************
+//*      Cosmic Muons Monte Carlo Simulation           *
+//*                                                    *
+//*      Alexis FAGOT                                  *
+//*      alexis.fagot@ugent.be                         *
+//*                                                    *
+//*      Janvier 16, 2016                              *
+//*                                                    *
+//*      HEADER FILE                                   *
+//******************************************************
+
+
+#include "../include/fonctions.h"
+#include <sstream>
+
+#include "TFile.h"
+#include "TH2F.h"
+
+using namespace std;
+
+int main(int argc ,char *argv[]){
+    stringstream converter;
+    converter << argv[0];
+    string program;
+    converter >> program;
+    converter.clear();
+
+    if(argc != 3){
+        cout << "Expects to have 3 parameters\n";
+        cout << "[Offline] USAGE is : " << program << " Doffset_low Doffset_high/n";
+        return -1;
+    } else if(argc == 3){
+        int Doffset_low = 0;
+        int Doffset_high = 0;
+
+        converter << argv[1];
+        converter >> Doffset_low;
+        converter.clear();
+
+        converter << argv[2];
+        converter >> Doffset_high;
+        converter.clear();
+
+        for(int d=Doffset_low; d<=Doffset_high; d++){
+            string ROOTName = "Angle-dependency_" + intTostring(d) + ".root";
+
+            TFile *outputFile = new TFile(ROOTName.c_str(),"recreate");
+
+            for(unsigned int a=0; a<60; a+=5){
+
+                //definition of the scintillator inclination angle
+                double alpha = a*PI/180.;
+
+                /** PROGRAM CODE **/
+                // Start with random seed
+                random_device rd;
+                Generator generator(rd());
+
+                // Get the thing started
+                generator.discard(100);
+
+                //Definitions of the coordinates and of the cosmic direction
+                Point muonPos, GroundPos, ScreenPos;
+                Direction direction;
+
+                //List of root IDs and root Titles for all triggered muons
+                string grd_profilID = "grd_prof_" + intTostring(a);
+                string grd_profilTitle = "Muon hit profil on the ground";
+                string scr_profilID = "scr_prof_" + intTostring(a);
+                string scr_profilTitle = "Muon hit profil on the vertical screen";
+                string muondirID = "direction_" + intTostring(a);
+                string muondirTitle = "Triggered muon direction components";
+
+                TH2F* GroundProfil = new TH2F(grd_profilID.c_str(),grd_profilTitle.c_str(),200,-100,100,200,-100,100);
+                TH2F* ScreenProfil = new TH2F(scr_profilID.c_str(),scr_profilTitle.c_str(),200,-100,100,200,-100,100);
+                TH2F* MuonDir = new TH2F(muondirID.c_str(),muondirTitle.c_str(),90,0.,90.,40,0.,360.);
+
+                //List of root IDs and root Titles for triggered forward muons
+                string grd_profilIDF = "grd_prof_f_" + intTostring(a);
+                string grd_profilTitleF = "Forward muon hit profil on the ground";
+                string scr_profilIDF = "scr_prof_f_" + intTostring(a);
+                string scr_profilTitleF = "Forward muon hit profil on the vertical screen";
+                string muondirIDF = "direction_f_" + intTostring(a);
+                string muondirTitleF = "Triggered forward muon direction components";
+
+                TH2F* GroundProfilF = new TH2F(grd_profilIDF.c_str(),grd_profilTitleF.c_str(),200,-100,100,200,-100,100);
+                TH2F* ScreenProfilF = new TH2F(scr_profilIDF.c_str(),scr_profilTitleF.c_str(),200,-100,100,200,-100,100);
+                TH2F* MuonDirF = new TH2F(muondirIDF.c_str(),muondirTitleF.c_str(),90,0.,90.,40,0.,360.);
+
+                //List of root IDs and root Titles for triggered backward muons
+                string grd_profilIDB = "grd_prof_b_" + intTostring(a);
+                string grd_profilTitleB = "Backward muon hit profil on the ground";
+                string scr_profilIDB = "scr_prof_b_" + intTostring(a);
+                string scr_profilTitleB = "Backward muon hit profil on the vertical screen";
+                string muondirIDB = "direction_b_" + intTostring(a);
+                string muondirTitleB = "Triggered backward muon direction components";
+
+                TH2F* GroundProfilB = new TH2F(grd_profilIDB.c_str(),grd_profilTitleB.c_str(),200,-100,100,200,-100,100);
+                TH2F* ScreenProfilB = new TH2F(scr_profilIDB.c_str(),scr_profilTitleB.c_str(),200,-100,100,200,-100,100);
+                TH2F* MuonDirB = new TH2F(muondirIDB.c_str(),muondirTitleB.c_str(),90,0.,90.,40,0.,360.);
+
+                unsigned int Nmuons = 1e3;  //The number of muons to generate
+                float Stat = float(Nmuons); //Number used for the progress bar only - is equal to Nmuons
+                float progress = 0.0;       //Estimation of the progress in percent for each threshold value
+
+                unsigned int n = 0;
+
+                while(n<Nmuons){
+                    muonPos = getRandomMuonPosition(generator,__scint_H+__scint_W*sin(alpha)/2.);
+                    direction = getRandomDirection(generator);
+
+                    bool in_trigger = isInScint(muonPos,direction,alpha,d,"TOP") && isInScint(muonPos,direction,alpha,d,"BOTTOM");
+
+                    if (in_trigger){
+                        GroundPos = getHitPosition(muonPos,direction,alpha,d,"GROUND");
+                        GroundProfil->Fill(GroundPos.x,GroundPos.y);
+
+                        ScreenPos = getHitPosition(muonPos,direction,alpha,d,"SCREEN");
+                        ScreenProfil->Fill(ScreenPos.x,ScreenPos.z);
+
+                        MuonDir->Fill(direction.first*180./PI, direction.second*180./PI);
+
+                        if(direction.second < PI){
+                            GroundProfilF->Fill(GroundPos.x,GroundPos.y);
+                            ScreenProfilF->Fill(ScreenPos.x,ScreenPos.z);
+                            MuonDirF->Fill(direction.first*180./PI, direction.second*180./PI);
+                        } else {
+                            GroundProfilB->Fill(GroundPos.x,GroundPos.y);
+                            ScreenProfilB->Fill(ScreenPos.x,ScreenPos.z);
+                            MuonDirB->Fill(direction.first*180./PI, direction.second*180./PI);
+                        }
+
+                        n++;
+                    }
+
+                    //Print progress bar
+                    progress = float(n/Stat);
+
+                    int barWidth = 70;
+
+                    cout << "Progress a-" << intTostring(a);
+                    cout << " [";
+                    int pos = barWidth * progress;
+                    for (int i = 0; i < barWidth; ++i) {
+                        if (i < pos) cout << "=";
+                        else if (i == pos) cout << ">";
+                        else cout << " ";
+                    }
+                    cout << "] " << int(progress * 100.0) << " %\r";
+                    cout.flush();
+                }
+                cout << endl;
+
+                //Write histos
+
+                GroundProfil->Write();
+                ScreenProfil->Write();
+                MuonDir->Write();
+
+                GroundProfilF->Write();
+                ScreenProfilF->Write();
+                MuonDirF->Write();
+
+                GroundProfilB->Write();
+                ScreenProfilB->Write();
+                MuonDirB->Write();
+
+                //Delete histos
+
+                delete GroundProfil;
+                delete ScreenProfil;
+                delete MuonDir;
+
+                delete GroundProfilF;
+                delete ScreenProfilF;
+                delete MuonDirF;
+
+                delete GroundProfilB;
+                delete ScreenProfilB;
+                delete MuonDirB;
+            }
+            outputFile->Close();
+        }
+    }
+
+    return 0;
+}
